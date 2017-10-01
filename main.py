3a54abd6e2e3e2d51aa3211865d03c65e6ef8426
ajinkya-rasane <ajinkyaashwin@gmail.com>
2017-10-02 00:25:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ except AttributeError:
         return QtGui.QApplication.translate(context, text, disambig)
 
 class Ui_Dialog(QtGui.QWidget):
-
+    
+    ## widget initialisation ##
     def __init__(self):
         super(Ui_Dialog,self).__init__()
         self.setupUi(self)
@@ -24,11 +25,13 @@ class Ui_Dialog(QtGui.QWidget):
         self.corpus_path = None
 
     def setupUi(self, Dialog):
+        ## set layout of widget ##
         Dialog.setObjectName(_fromUtf8("Dialog"))
         Dialog.resize(407, 431)
         self.verticalLayout_2 = QtGui.QVBoxLayout(Dialog)
         self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
-
+        
+        ## create labels ##
         self.label = QtGui.QLabel(Dialog)
         font = QtGui.QFont()
         font.setPointSize(10)
@@ -36,7 +39,9 @@ class Ui_Dialog(QtGui.QWidget):
         self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.label.setObjectName(_fromUtf8("label"))
         self.verticalLayout_2.addWidget(self.label)
-
+        
+        
+        ## create radio buttons ##
         self.static = QtGui.QRadioButton('static corpus', self)
         self.dynamic = QtGui.QRadioButton('Dynamic corpus (google search, will be slower)', self)
         self.searchType = QtGui.QButtonGroup()
@@ -47,12 +52,14 @@ class Ui_Dialog(QtGui.QWidget):
 
         self.verticalLayout = QtGui.QVBoxLayout()
         self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
-
+        
+        ## create checkbox ##
         self.chkBoxItem = QtGui.QCheckBox()
         self.chkBoxItem.setCheckState(QtCore.Qt.Unchecked)
         self.chkBoxItem.setText('Preprocess corpus')
         self.verticalLayout.addWidget(self.chkBoxItem)
-
+        
+        ##create push buttons ##
         self.pushButton = QtGui.QPushButton(Dialog)
         self.pushButton.setObjectName(_fromUtf8("pushButton"))
         self.verticalLayout.addWidget(self.pushButton)
@@ -72,6 +79,7 @@ class Ui_Dialog(QtGui.QWidget):
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
+        ## binding ui elements with their functions ##
         Dialog.setWindowTitle("Plagiarism Checker")
         self.label.setText(_translate("Dialog", "enter the document and corpus location", None))
         self.pushButton.setText(_translate("Dialog", "document", None))
@@ -84,14 +92,17 @@ class Ui_Dialog(QtGui.QWidget):
         self.static.clicked.connect(self.staticClicked)
 
         self.setWindowIcon(QtGui.QIcon('plagiarism-image.png'))
-
+    
+    
+    ## function to browse the document ##
     def docBrowse(self):
         self.target_path = QtGui.QFileDialog.getOpenFileName(self,
                                                           'Single File',
                                                           "C:/",'*')
         self.pushButton.setText("Document: " + self.target_path)
         print(self.target_path)
-    #select corpus folder
+        
+    ## function to select the corpus folder ##
     def corpBrowse(self):
         self.corpus_path = QtGui.QFileDialog.getExistingDirectory(None,
                                                            'Select a folder:',
@@ -99,7 +110,8 @@ class Ui_Dialog(QtGui.QWidget):
                                                            QtGui.QFileDialog.ShowDirsOnly)
         self.pushButton_2.setText("Corpus: " + self.corpus_path)
         print(self.corpus_path)
-
+        
+    ## function to run the tf-idf code ##
     def runCode(self):
         if self.target_path == None or len(self.target_path) < 1:
             print('Please select a target document for checking!')
@@ -131,6 +143,7 @@ class Ui_Dialog(QtGui.QWidget):
         self.pushButton_2.show()
         self.chkBoxItem.show()
 
+## main method to create the gui app ##
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
     ex = Ui_Dialog()
